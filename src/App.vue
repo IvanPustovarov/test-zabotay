2b3acc133cdfef0a54a581e035158df8ca7af41d
John <silverflash150@gmail.com>
2023-01-31 02:01:43 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+  <!-- <button class="delete"></button> -->
+</template>
+
+<script>
+import '@/scss/mystyles.scss'
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/LeaderBoard.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script>
+// import '@/scss/mystyles.scss'
+
+export default {
+  name: 'LeaderBoard',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
